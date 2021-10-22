2530933ceb823ae2db4b39b36e59e3839b459fe5
david.schruf <david.schruf@wikifolio.com>
2021-10-22 08:13:51 +0200

    remove superflous lines

--- DevToolsSessionCloseCrashes/Program.cs
+++ DevToolsSessionCloseCrashes/Program.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.DevTools;
 
 using (var driver = new ChromeDriver())
 {
-    var devToolsDriver = driver as IDevTools;
     driver.Url = "https://www.selenium.dev";
     var devToolsSession = driver.GetDevToolsSession();
     driver.Close();
